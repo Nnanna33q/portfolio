d6ef5215f99645ffceef75df28e70fc0b35e1216
Hakeem <hakeem@Hakeems-MacBook-Pro.local>
2025-06-27 03:42:05 +0100

    Mobile navbar ui fix

--- src/components/mobileNavbar.jsx
+++ src/components/mobileNavbar.jsx
@@ -3,15 +3,15 @@ import { useRef } from "react";
 
 export default function MobileNavbar({ currentPage }) {
     const navbarHeightRef = useRef(document.querySelector('.navbar-container').clientHeight);
-
+    
     return (
         <motion.div
             initial={{ x: '-100vw' }}
             animate={{ x: 0 }}
             transition={{ bounce: 0, duration: 0.3, animationTimingFunction: 'ease-in-out' }}
             exit={{ x: '-100vw' }}
-            className={`mobile-nav-container bg-primary dark:bg-secondary fixed bottom-0 left-0 overflow-hidden z-100`}
-            style={{ width: '100vw', height: `calc(100vh - ${navbarHeightRef.current}px)` }}>
+            className={`mobile-nav-container bg-primary dark:bg-secondary overflow-hidden z-100`}
+            style={{ width: '100vw', height: `calc(100vh - ${navbarHeightRef.current}px)`, position: 'fixed', top: `${navbarHeightRef.current}px`, left: 0 }}>
             <ul className="flex flex-col justify-evenly items-center text-secondary dark:text-primary h-full">
                 <li>
                     <a href="index.html" className="text-2xl md:text-3xl font-clashbold">Home</a>
